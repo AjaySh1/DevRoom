90ba44ff999c05ec5a522ece447b94eb620db243
Ajay Sharma <124371020+AjaySh1@users.noreply.github.com>
2025-08-09 12:45:31 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://dev-room-8sa2.vercel.app/",
   credentials: true
 }));
 
