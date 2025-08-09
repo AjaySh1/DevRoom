99580060db1e1895146126251d3734a1fd360cdc
AjaySh1 <ajaysharmapns1234@gmail.com>
2025-08-09 11:11:37 +0530

    upd

--- backend/tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-app.post('/convert-code', async (req, res) => {
-    try {
-        const { code, targetLanguage } = req.body;
-        const convertedCode = await processCodeWithGenerativeAI(code, `Convert the following code to ${targetLanguage}:   Provide only the converted code, no headings, no explanations, and no additional text. Ensure proper formatting and do not include any comments or other text.
-`);
